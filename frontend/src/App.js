eab97e23853718d8239f518a092e3f488f0f9b3b
VidushaLiyangolla <vidusha.nirmani@gmail.com>
2022-05-07 11:36:44 +0530

    Product details section.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,29 @@
-import React, {Fragment} from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import './App.css'
-import Landing from './components/layout/Landing'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import Profile from './components/profile/Profile'
-import Dashboard from './components/admin/Dashboard'
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+import Feedback from "./components/feedback/feedabck";
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Profile from './components/profile/Profile';
+import Dashboard from './components/admin/Dashboard';
 import Products from './components/products/Products';
 
 // Redux
-import {Provider} from 'react-redux'
-import store from './store'
-import Product from "./components/layout/productSection/Product";
+import {Provider} from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
-          <Route exact path='/' component={Landing}/>
-          <Route exact path='/products' component={Products}/>
           <Switch>
+            <Route exact path='/' component={Landing}/>
             <Route exact path='/register' component={Register}/>
             <Route exact path='/login' component={Login}/>
+            <Route exact path='/products' component={Products}/>
+            <Route exact path='/product-details/:_id' component={Feedback} />
             <Route exact path='/profile' component={Profile}/>
             <Route exact path='/admin' component={Dashboard}/>
           </Switch>
@@ -32,4 +33,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
